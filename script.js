4dbed747fa1aa9319a0ea47d7415de458c3aeec6
Frank <frankkashere@gmail.com>
2023-10-12 07:27:00 +0300

    Update 3

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const h2 = document.createElement('h2');
 h2.textContent = `This content is added by JavaScript`;
 
-const h3 = document.createElement('h3');
-h3.textContent = "Welcome to JavaScript";
-
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement('h3');
+h3.textContent = `Welcome to JavaScript`;
+
 document.querySelector("body").appendChild(h3);
